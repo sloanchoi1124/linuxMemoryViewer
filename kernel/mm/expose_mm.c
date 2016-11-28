0a16c8e10f3af9395283189ae60683a898cdf1a5
sloanchoi1124 <xc2315@barnard.edu>
2016-11-28 03:29:43 -0500

    checkpatch

--- kernel/mm/expose_mm.c
+++ kernel/mm/expose_mm.c
@@ -5,8 +5,8 @@
 #include <linux/syscalls.h>
 #include <asm/page.h>
 #include <asm/memory.h>
-#include <asm/uaccess.h>
-
+#include <linux/uaccess.h>
+//TODO: change it back to asm if it doesn't work
 struct walk_info {
 	pgd_t *kernel_fake_pgd_base;
 	int pmd_counter;
@@ -18,22 +18,22 @@ struct walk_info {
 };
 
 
-int my_pgd_entry(pgd_t *pgd, unsigned long addr, unsigned long next, 
+int my_pgd_entry(pgd_t *pgd, unsigned long addr, unsigned long next,
 	      struct mm_walk *walk)
 {
 	unsigned long pgd_index = pgd_index(addr);
 	struct walk_info *my_walk_info = (struct walk_info *)walk->private;
-	pmd_t* new_pmd;
+	pmd_t *new_pmd;
 	int cnt;
 	pgd_t *current_kernel_pgd_base =
 		my_walk_info->kernel_fake_pgd_base;
 
-	
+
 	*(current_kernel_pgd_base + pgd_index) =
 		my_walk_info->last_written_pmd_val;
 	my_walk_info->last_written_pmd_val += PAGE_SIZE;
-	new_pmd = (pmd_t *) kcalloc(1, PAGE_SIZE, GFP_KERNEL);
-	if (new_pmd == NULL) 
+	new_pmd = kcalloc(1, PAGE_SIZE, GFP_KERNEL);
+	if (new_pmd == NULL)
 		return -ENOMEM;
 	cnt = my_walk_info->pmd_counter;
 	my_walk_info->pmd_buf[cnt] = new_pmd;
@@ -41,14 +41,14 @@ int my_pgd_entry(pgd_t *pgd, unsigned long addr, unsigned long next,
 	return 0;
 }
 
-int my_pmd_entry(pmd_t *pmd, unsigned long addr, unsigned long next, 
+int my_pmd_entry(pmd_t *pmd, unsigned long addr, unsigned long next,
 	      struct mm_walk *walk)
-{	
+{
 	unsigned long pmd_index = pmd_index(addr);
 	struct walk_info *my_walk_info = (struct walk_info *)walk->private;
-	
+
 	unsigned long current_pte_base = my_walk_info->last_written_pte_val;
-	struct vm_area_struct *user_vma = 
+	struct vm_area_struct *user_vma =
 		find_vma(current->mm, current_pte_base);
 
 	unsigned long pfn;
@@ -59,23 +59,21 @@ int my_pmd_entry(pmd_t *pmd, unsigned long addr, unsigned long next,
 
 	if (user_vma == NULL)
 		return -EINVAL;
-	
-	if (unlikely(user_vma->vm_start != current_pte_base)) {
+
+	if (unlikely(user_vma->vm_start != current_pte_base))
 		return -EFAULT;
-	}
-	if (unlikely(user_vma->vm_end != current_pte_base + PAGE_SIZE)) {
+	if (unlikely(user_vma->vm_end != current_pte_base + PAGE_SIZE))
 		return -EFAULT;
-	}
 
 	if (pmd == NULL)
 		return 0;
 
 	pfn = page_to_pfn(pmd_page(*pmd));
-	if (pmd_bad(*pmd) || !pfn_valid(pfn)) 
+	if (pmd_bad(*pmd) || !pfn_valid(pfn))
 		return -EINVAL;
-	
+
 	err = 0;
-	err = remap_pfn_range(user_vma, current_pte_base, 
+	err = remap_pfn_range(user_vma, current_pte_base,
 		pfn, PAGE_SIZE, user_vma->vm_page_prot);
 	if (err)
 		return err;
@@ -86,8 +84,8 @@ int my_pmd_entry(pmd_t *pmd, unsigned long addr, unsigned long next,
 	return 0;
 }
 
-SYSCALL_DEFINE2(get_pagetable_layout, struct pagetable_layout_info __user *, 
-		pgtbl_info, int, size) 
+SYSCALL_DEFINE2(get_pagetable_layout, struct pagetable_layout_info __user *,
+		pgtbl_info, int, size)
 {
 	struct pagetable_layout_info layout_info;
 	if (size < sizeof(struct pagetable_layout_info))
@@ -97,19 +95,19 @@ SYSCALL_DEFINE2(get_pagetable_layout, struct pagetable_layout_info __user *,
 	layout_info.page_shift = PAGE_SHIFT;
 	if (copy_to_user(pgtbl_info, &layout_info, size))
 		return -EFAULT;
-	
+
 	return 0;
 
 }
 
 SYSCALL_DEFINE6(expose_page_table, pid_t, pid, unsigned long, fake_pgd,
-		unsigned long, fake_pmds, unsigned long, page_table_addr, 
-		unsigned long, begin_vaddr, unsigned long, end_vaddr) 
+		unsigned long, fake_pmds, unsigned long, page_table_addr,
+		unsigned long, begin_vaddr, unsigned long, end_vaddr)
 {
 	struct mm_walk walk;
 	struct walk_info *my_walk_info;
 	struct task_struct *target_tsk;
-	pgd_t * kernel_pgd_base;
+	pgd_t *kernel_pgd_base;
 	struct vm_area_struct *pgd_vma;
 	struct vm_area_struct *pmd_vma;
 	struct vm_area_struct *pte_vma;
@@ -129,7 +127,7 @@ SYSCALL_DEFINE6(expose_page_table, pid_t, pid, unsigned long, fake_pgd,
 	if (my_walk_info == NULL)
 		return -ENOMEM;
 
-	kernel_pgd_base = (pgd_t *) kcalloc(1, PAGE_SIZE, GFP_KERNEL);
+	kernel_pgd_base = kcalloc(1, PAGE_SIZE, GFP_KERNEL);
 	if (kernel_pgd_base == NULL) {
 		err = -ENOMEM;
 		goto free_walk_info;
@@ -140,26 +138,26 @@ SYSCALL_DEFINE6(expose_page_table, pid_t, pid, unsigned long, fake_pgd,
 	down_write(&current->mm->mmap_sem);
 	if (pid != -1)
 		down_write(&target_tsk->mm->mmap_sem);
-	
+
 	pgd_vma = find_vma(current->mm, fake_pgd);
-	if (pgd_vma == NULL){
+	if (pgd_vma == NULL) {
 		err = -EINVAL;
 		goto sem_release;
 	}
 	if (pgd_vma->vm_start < fake_pgd) {
-		if (split_vma(current->mm, pgd_vma, fake_pgd, 1)){
+		if (split_vma(current->mm, pgd_vma, fake_pgd, 1)) {
 			err = -EFAULT;
 			goto sem_release;
 		}
 	}
 	pmd_vma = find_vma(current->mm, fake_pmds);
-	if (pmd_vma == NULL){
+	if (pmd_vma == NULL) {
 		err = -EINVAL;
 		goto sem_release;
 	}
 
 	if (pmd_vma->vm_start < fake_pmds) {
-		if (split_vma(current->mm, pmd_vma, fake_pmds, 1)){
+		if (split_vma(current->mm, pmd_vma, fake_pmds, 1)) {
 			err = -EFAULT;
 			goto sem_release;
 		}
@@ -170,12 +168,12 @@ SYSCALL_DEFINE6(expose_page_table, pid_t, pid, unsigned long, fake_pgd,
 		goto sem_release;
 	}
 	if (pte_vma->vm_start < page_table_addr) {
-		if (split_vma(current->mm, pte_vma, page_table_addr, 1)){
+		if (split_vma(current->mm, pte_vma, page_table_addr, 1)) {
 			err = -EFAULT;
 			goto sem_release;
 		}
 	}
-	
+
 	pte_vma->vm_flags &= ~VM_WRITE;
 	pte_vma->vm_page_prot = vm_get_page_prot(pte_vma->vm_flags);
 
@@ -204,7 +202,7 @@ SYSCALL_DEFINE6(expose_page_table, pid_t, pid, unsigned long, fake_pgd,
 	if (err)
 		goto free_kpmd_buffer;
 
-	
+
 	if (copy_to_user((unsigned long *) fake_pgd, (unsigned long *)
 		     kernel_pgd_base, PAGE_SIZE)){
 		err = -ENOMEM;
@@ -213,15 +211,15 @@ SYSCALL_DEFINE6(expose_page_table, pid_t, pid, unsigned long, fake_pgd,
 
 	for (i = 0; i < my_walk_info->pmd_counter; i++) {
 		unsigned long result;
-		pmd_t * source;
-		unsigned long * dest;
+		pmd_t *source;
+		unsigned long *dest;
 		source = my_walk_info->pmd_buf[i];
 		dest = (unsigned long *) (fake_pmds + i * PAGE_SIZE);
 		result = copy_to_user(dest, source, PAGE_SIZE);
-		if (result){
+		if (result) {
 			err = -ENOMEM;
 			goto free_kpmd_buffer;
-		} 
+		}
 	}
 free_kpmd_buffer:
 	for (i = 0; i < my_walk_info->pmd_counter; i++)
@@ -238,7 +236,7 @@ free_kpgd_buffer:
 
 free_walk_info:
 	kfree(&my_walk_info);
-		
+
 	return err;
 
 }
--- user/test/vm_inspector.c
+++ user/test/vm_inspector.c
@@ -14,13 +14,13 @@
 #define PTRS_PER_PMD	512
 #define PTRS_PER_PTE	512
 #define PAGE_SIZE	4096
-#define PTE_VALID	1UL << 0
-#define PTE_PROC_NONE	1UL << 1
-#define PTE_DIRTY	1UL << 55
-#define PTE_RDONLY	1UL << 7
-#define PTE_AF		1UL << 10
-#define PTE_UXN		1UL << 54
-#define PTE_FILE	1UL << 2
+#define PTE_VALID	(1UL << 0)
+#define PTE_PROC_NONE	(1UL << 1)
+#define PTE_DIRTY	(1UL << 55)
+#define PTE_RDONLY	(1UL << 7)
+#define PTE_AF		(1UL << 10)
+#define PTE_UXN		(1UL << 54)
+#define PTE_FILE	(1UL << 2)
 #define pte_present(pte)	(!!(pte & (PTE_VALID | PTE_PROC_NONE)))
 #define pte_young(pte)		(!!(pte & (PTE_AF)))
 #define pte_file(pte)		(!!(pte & (PTE_FILE)))
@@ -28,31 +28,31 @@
 #define pte_read_only(pte)	(!!(pte & (PTE_RDONLY)))
 #define pte_uxn(pte)		(!!(pte & (PTE_UXN)))
 
-unsigned long get_phys()
+unsigned long get_phys(void)
 {
 	unsigned long tmp = 1;
 	return (tmp << 40) - 1;
 }
 
 
-int pgd_index(unsigned long addr) 
+int pgd_index(unsigned long addr)
 {
-	return (((addr) >> PGDIR_SHIFT) & (PTRS_PER_PGD - 1));
+	return ((addr) >> PGDIR_SHIFT) & (PTRS_PER_PGD - 1);
 }
 
-int pmd_index(unsigned long addr) 
+int pmd_index(unsigned long addr)
 {
-	return (((addr) >> PMD_SHIFT) & (PTRS_PER_PMD - 1));
+	return ((addr) >> PMD_SHIFT) & (PTRS_PER_PMD - 1);
 }
 int pte_index(unsigned long addr)
 {
-	return (((addr) >> PAGE_SHIFT) & (PTRS_PER_PTE - 1));
+	return ((addr) >> PAGE_SHIFT) & (PTRS_PER_PTE - 1);
 }
 int pa_offset(unsigned long addr)
 {
 	return addr & (PAGE_SIZE - 1);
 }
-int main(int argc, char ** argv)
+int main(int argc, char **argv)
 {
 	int err;
 
@@ -61,7 +61,7 @@ int main(int argc, char ** argv)
 	unsigned long begin_vaddr;
 	unsigned long end_vaddr;
 	int verbose;
-	
+
 	size_t pgd_len = sysconf(_SC_PAGE_SIZE) * 4096;
 	unsigned long fake_pgd_base;
 	unsigned long fake_pmd_base;
@@ -76,7 +76,7 @@ int main(int argc, char ** argv)
 	if (argc == 5) {
 		pid = strtol(argv[2], NULL, 10);
 		if (strncmp("-v", argv[1], 2) != 0) {
-			printf("Usage: ./vm_inspector -v pid va_begin va_end\n");
+			printf("Usage:./vm_inspector -v pid va_begin va_end\n");
 			exit(1);
 		}
 		verbose = 1;
@@ -96,9 +96,10 @@ int main(int argc, char ** argv)
 		printf("invalid begin_vaddr and end_vaddr\n");
 		exit(1);
 	}
-	
+
 	err = 0;
-	tmp = mmap(NULL, pgd_len, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+	tmp = mmap(NULL, pgd_len,
+		   PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
 	if (tmp == MAP_FAILED) {
 		perror("line 69: mmap fail\n");
 		err = 1;
@@ -107,7 +108,8 @@ int main(int argc, char ** argv)
 	fake_pgd_base = (unsigned long)tmp;
 	fake_pgd_max = fake_pgd_base + pgd_len;
 
-	tmp = mmap(NULL, pgd_len, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+	tmp = mmap(NULL, pgd_len,
+		   PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
 	if (tmp == MAP_FAILED) {
 		perror("line 77:  mmap fail\n");
 		err = 1;
@@ -116,7 +118,8 @@ int main(int argc, char ** argv)
 	fake_pmd_base = (unsigned long)tmp;
 	fake_pmd_max = fake_pmd_base + pgd_len;
 
-	tmp = mmap(NULL, pgd_len, PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
+	tmp = mmap(NULL, pgd_len,
+		   PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
 	if (tmp == MAP_FAILED) {
 		perror("line 85: mmap fail\n");
 		err = 1;
@@ -125,8 +128,9 @@ int main(int argc, char ** argv)
 
 	page_table_addr = (unsigned long)tmp;
 	fake_pte_max = page_table_addr + pgd_len;
-	
-	if (syscall(245, pid, fake_pgd_base, fake_pmd_base, page_table_addr, begin_vaddr, end_vaddr)) {
+
+	if (syscall(245, pid, fake_pgd_base, fake_pmd_base,
+		    page_table_addr, begin_vaddr, end_vaddr)) {
 		perror("syscall 245 failed!\n");
 		err = 1;
 		goto free_pte;
@@ -143,14 +147,16 @@ int main(int argc, char ** argv)
 	unsigned long table_entry;
 	unsigned long phys_mask;
 	phys_mask = get_phys();
-	
+
 	current_va = begin_vaddr;
-	while(current_va <= end_vaddr) {
+	while (current_va <= end_vaddr) {
 		cur_pgd_index = pgd_index(current_va);
-		if ((fake_pgd_base + cur_pgd_index * ENTRY_SIZE) >= fake_pgd_max)
+		if ((fake_pgd_base + cur_pgd_index * ENTRY_SIZE)
+		    >= fake_pgd_max)
 			goto next_va;
 
-		cur_fake_pmd = *((unsigned long *)(fake_pgd_base + cur_pgd_index * ENTRY_SIZE));
+		cur_fake_pmd = *((unsigned long *)
+				 (fake_pgd_base + cur_pgd_index * ENTRY_SIZE));
 		if (cur_fake_pmd == 0)
 			goto next_va;
 
@@ -158,7 +164,8 @@ int main(int argc, char ** argv)
 		if ((cur_fake_pmd + cur_pmd_index * ENTRY_SIZE) >= fake_pmd_max)
 			goto next_va;
 
-		cur_fake_pte = *((unsigned long *)(cur_fake_pmd + cur_pmd_index * ENTRY_SIZE));
+		cur_fake_pte = *((unsigned long *)
+				 (cur_fake_pmd + cur_pmd_index * ENTRY_SIZE));
 		if (cur_fake_pte == 0)
 			goto next_va;
 
@@ -166,25 +173,30 @@ int main(int argc, char ** argv)
 			goto next_va;
 
 		cur_pte_index = pte_index(current_va);
-		table_entry = *((unsigned long *)(cur_fake_pte + cur_pte_index * ENTRY_SIZE));
-		
+		table_entry = *((unsigned long *)
+				(cur_fake_pte + cur_pte_index * ENTRY_SIZE));
+
 		if (table_entry == 0)
 			goto next_va;
 
-		if (pte_present(table_entry) == 0) 
+		if (pte_present(table_entry) == 0)
 			goto next_va;
-			
+
 		cur_pa_base = (table_entry & phys_mask) >> PAGE_SHIFT;
 		cur_pa = (cur_pa_base << PAGE_SHIFT) + pa_offset(current_va);
-		printf("0x%lx\t0x%lx\t%d\t%d\t%d\t%d\t%d\n", current_va, cur_pa,
-		       pte_young(table_entry), pte_file(table_entry),
-		       pte_dirty(table_entry), pte_read_only(table_entry),
+		printf("0x%lx\t0x%lx\t%d\t%d\t%d\t%d\t%d\n",
+		       current_va, cur_pa,
+		       pte_young(table_entry),
+		       pte_file(table_entry),
+		       pte_dirty(table_entry),
+		       pte_read_only(table_entry),
 		       pte_uxn(table_entry));
 		current_va += 4096;
 		continue;
-next_va:	
+next_va:
 		if (verbose)
-			printf("0x%lx\t0x%lx\t%d\t%d\t%d\t%d\t%d\n", current_va, 
+			printf("0x%lx\t0x%lx\t%d\t%d\t%d\t%d\t%d\n",
+			       current_va,
 			       cur_pa, 0, 0, 0, 0, 0);
 		current_va += 4096;
 	}
